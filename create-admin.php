f39e9c968aa6fe836914e1ddcadc859124fcfd1a
localgrowdigital-tech <YOUR_GITHUB_EMAIL>
2026-07-19 14:34:08 +0530

    Remove temporary admin password generator

--- create-admin.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-$password = '12345';
-$passwordHash = password_hash($password, PASSWORD_DEFAULT);
-
-echo '<h2>Admin Password Hash</h2>';
-echo '<textarea style="width:100%;height:120px;">';
-echo htmlspecialchars($passwordHash, ENT_QUOTES, 'UTF-8');
-echo '</textarea>';
